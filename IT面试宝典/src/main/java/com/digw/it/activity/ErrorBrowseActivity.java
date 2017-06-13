8ca4563314e5f84ad2c4331381bb2490fc8741d6
liu <lcl865992776@hotmail.com>
2017-06-14 00:23:30 +0800

    liu 2017-6-14 0:23

--- IT面试宝典/src/main/java/com/digw/it/activity/ErrorBrowseActivity.java
+++ IT面试宝典/src/main/java/com/digw/it/activity/ErrorBrowseActivity.java
@@ -2,6 +2,7 @@ package com.digw.it.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.digw.it.R;
 //错误浏览
@@ -10,6 +11,7 @@ public class ErrorBrowseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_error_browse);
     }
 }
