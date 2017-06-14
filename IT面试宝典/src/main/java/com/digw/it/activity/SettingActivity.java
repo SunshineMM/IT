4d2cd787300246edea0af7bd6232b095b634dcac
qq1023308356 <1023308356@qq.com>
2017-06-14 09:19:03 +0800

    你好

--- IT面试宝典/src/main/java/com/digw/it/activity/SettingActivity.java
+++ IT面试宝典/src/main/java/com/digw/it/activity/SettingActivity.java
@@ -1,10 +1,17 @@
 package com.digw.it.activity;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.digw.it.R;
 
@@ -20,6 +27,15 @@ public class SettingActivity extends BaseActivity {
 
     @Override
     public void initParams(Bundle bundle) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
@@ -47,6 +63,9 @@ public class SettingActivity extends BaseActivity {
     public void doBusiness(Context mContext) {
         getFragmentManager().beginTransaction().replace(R.id.content_frame, new SettingFragment()).commit();
         SettingFragment frag = (SettingFragment) getFragmentManager().findFragmentById(R.id.content_frame);
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        Log.e("666",sp.getBoolean("wifi",false)+"");
+
     }
 
 
